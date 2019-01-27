033a7e8f0ec4003c697b0d722864ab0e8eae14a0
abc <abc@example.com>
2019-01-27 19:43:18 +0300

    app structure modified

---
+++ app/config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config(object):
+	"""docstring for config"""
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'cannot decode string'
+
+
--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,15 @@ from app import app
 def index():
 
 	user = {"username": "martin"}
-	return render_template("index.html", title="Home" user=user) 
+	posts = [
+		{
+			"author":{"username": "john"},
+			"body": "Beauty lies in the eyes of the beholder"
+		},
+		{
+			"author":{"username": "lolo"},
+			"body": "life is a journey"
+		},	
+	]
+		
+	return render_template("index.html", title="Home", user=user,  posts=posts) 
