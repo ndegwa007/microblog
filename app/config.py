88ef0849a732d25e47cc2f6e35172cd8b430ede4
abc <abc@example.com>
2019-02-22 17:44:15 +0300

    initial app setup

--- app/config.py
+++ app/config.py
@@ -1,7 +1,12 @@
 import os
-
+basedir = os.path.abspath(os.basedir.path(__file__))
 class Config(object):
 	"""docstring for config"""
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'cannot decode string'
+	SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL') or \
+		'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
 
 
